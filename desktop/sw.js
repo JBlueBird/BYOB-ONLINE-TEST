5300c057ed31e0aab364e7fffae721adc3e6e1b4
JBlueBird <incJosiah@gmail.com>
2025-06-04 13:31:10 -0500

    Added the webapp test

---
+++ desktop/sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'byob-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
